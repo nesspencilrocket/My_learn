8df49e133a3923c303dcef7709cb14b408dc706a
nesspencilrocket <akihiro.k2608@gmail.com>
2025-12-24 17:30:57 +0900

    Create 練習コード01途中.c

---
+++ 02_C/練習コード01途中.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void attack_demon(int *demon_hp);
+void use_potion(int *hero_hp, int type);
+
+int main(void){
+    srand((unsigned int)time(NULL);)
+
+}
